549ca4a65ab80467a9c7378f0bd9ff6a33bbc8c2
PrakharSingh0908 <102904893+PrakharSingh0908@users.noreply.github.com>
2022-12-17 01:14:18 +0530

    Next with Sanity

---
+++ sanityecommerce/schemas/product.js
@@ -0,0 +1,12 @@
+export default {
+    name: 'product',
+    title: 'Product',
+    type: 'document',
+    fields: [
+        {
+            name : 'name',
+            title : 'Name',
+            type : 'string',
+        }
+    ]
+}
