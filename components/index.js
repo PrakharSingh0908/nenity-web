e3d842d8fe16092c2692f4104a73402c8145ebb5
PrakharSingh0908 <102904893+PrakharSingh0908@users.noreply.github.com>
2022-12-16 20:26:38 +0530

    Components added

---
+++ components/index.js
@@ -0,0 +1,7 @@
+export { default as Footer} from './Footer';
+export { default as Layout} from './Layout';
+export { default as Navbar} from './Navbar';
+export { default as Product} from './Product';
+export { default as Herobanner} from './HeroBanner';
+export { default as Footerbanner} from './FooterBanner';
+export { default as Cart} from './Cart';
--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ const Home = () => {
 					<h2>Best selling products</h2>
 					<p>Speakers of many variations</p>
 				</div>
-				<div>
+				<div className="products-container">
 					{["Product 1", "Product 2"].map((product) => product)}
 				</div>
 				Footer
